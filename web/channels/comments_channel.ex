aa5ccf5d999f3e5e43e10c404c838fda958c6f89
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2025-06-09 09:12:52 -0500

    learn how sockets works

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,11 +1,16 @@
 defmodule Discuss.CommentsChannel do
-  use Disscuss.Web, :channel
+  use Discuss.Web, :channel
 
-  def join() do
-
+  def join(name, _params, socket) do
+    {:ok, %{hey: "there"} ,socket}
   end
 
-  def handle_in() do
+  def handle_in(name, message, socket) do
+    IO.puts(String.duplicate("xx", 100))
+    IO.inspect(name)
+    IO.puts(String.duplicate("xx", 100))
+    IO.inspect(message)
 
+    {:reply, :ok, socket}
   end
 end
